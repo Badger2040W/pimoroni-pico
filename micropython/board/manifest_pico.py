22d055055b53e22383415fbb18fa908557e2c0d6
Phil Howard <github@gadgetoid.com>
2024-03-25 10:26:21 +0000

    Experimental: Attempt to build USB firmware.

--- micropython/board/manifest_pico.py
+++ micropython/board/manifest_pico.py
@@ -3,3 +3,10 @@ freeze("../modules_py", "interstate75.py")
 
 freeze("../modules_py", "pimoroni.py")
 freeze("../modules_py", "boot.py")
+
+require("usb-device")
+require("usb-device-hid")
+require("usb-device-cdc")
+# require("usb-device-keyboard")
+# require("usb-device-mouse")
+# require("usb-device-midi")
